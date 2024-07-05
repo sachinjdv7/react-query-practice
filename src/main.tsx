b00518f21d00c8d16433f910271bed86df2e522b
sachinjadhav <sachinjdv7@gmail.com>
2024-07-05 22:18:39 +0530

    error handling

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App.tsx";
 import "./index.css";
-
+import "bootstrap/dist/css/bootstrap.css";
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
