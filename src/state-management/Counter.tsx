be9f62d14cf48be7fc2dec391a6b787e3d63861c
sachinjadhav <sachinjdv7@gmail.com>
2024-07-08 23:02:53 +0530

    learn useReducer

---
+++ src/state-management/Counter.tsx
@@ -0,0 +1,33 @@
+import { useReducer } from "react";
+import counterReducer from "./Reducer/countReducer";
+
+const Counter = () => {
+  // const [value, setValue] = useState(0);
+  const [value, dispatch] = useReducer(counterReducer, 0);
+
+  return (
+    <div>
+      Counter ({value})
+      <button
+        onClick={() => dispatch({ type: "INCREMENT" })}
+        className="btn btn-primary mx-1"
+      >
+        Increment
+      </button>
+      <button
+        onClick={() => dispatch({ type: "DECREMENT" })}
+        className="btn btn-primary mx-1"
+      >
+        Decrement
+      </button>
+      <button
+        onClick={() => dispatch({ type: "RESET" })}
+        className="btn btn-primary mx-1"
+      >
+        Reset
+      </button>
+    </div>
+  );
+};
+
+export default Counter;
