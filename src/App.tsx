74b3c8b4e776d62018aa54c79dd516910c018716
sachinjadhav <sachinjdv7@gmail.com>
2024-07-09 10:48:18 +0530

    create AuthProvide and TaskProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,17 @@
-import { useReducer } from "react";
 import "./App.css";
+import AuthProvider from "./state-management/AuthProvider";
 import NavBar from "./state-management/NavBar";
-import tasksReducer from "./state-management/Reducer/tasksReducer";
 import TaskList from "./state-management/TaskList";
-import TasksContext from "./state-management/contexts/tasksContext";
-import authReducer from "./state-management/Reducer/authReducer";
-import AuthContext from "./state-management/contexts/authContext";
+import TaskProvider from "./state-management/TaskProvider";
 
 function App() {
-  const [tasks, taskDispatch] = useReducer(tasksReducer, []);
-  const [user, authDispatch] = useReducer(authReducer, "");
-
   return (
-    <AuthContext.Provider value={{ user, dispatch: authDispatch }}>
-      <TasksContext.Provider value={{ tasks, dispatch: taskDispatch }}>
+    <AuthProvider>
+      <TaskProvider>
         <NavBar />
         <TaskList />
-      </TasksContext.Provider>
-    </AuthContext.Provider>
+      </TaskProvider>
+    </AuthProvider>
   );
 }
 
