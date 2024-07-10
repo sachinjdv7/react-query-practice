e98e5dc0e7cd83ebcc6617687f4432a451fa8bdc
sachinjadhav <sachinjdv7@gmail.com>
2024-07-10 19:07:28 +0530

    counter store using zustand

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import "./App.css";
 
 import NavBar from "./state-management/NavBar";
 import { AuthProvider } from "./state-management/auth";
+import Counter from "./state-management/counter/Counter";
 import { TaskList, TaskProvider } from "./state-management/tasks";
 
 function App() {
   return (
     <AuthProvider>
       <TaskProvider>
+        <Counter />
         <NavBar />
         <TaskList />
       </TaskProvider>
--- src/state-management/counter/Counter.tsx
+++ src/state-management/counter/Counter.tsx
@@ -1,38 +1,14 @@
-import { useReducer } from "react";
-
-interface Action {
-  type: "INCREMENT" | "RESET" | "DECREMENT";
-}
-
-const counterReducer = (state: number, action: Action): number => {
-  if (action.type === "INCREMENT") return state + 1;
-  if (action.type === "DECREMENT") return state - 1;
-  if (action.type === "RESET") return 0;
-  return state;
-};
+import useCounterStore from "./store";
 
 const Counter = () => {
-  const [value, dispatch] = useReducer(counterReducer, 0);
-
+  const { counter, increment, reset } = useCounterStore();
   return (
     <div>
-      Counter ({value})
-      <button
-        onClick={() => dispatch({ type: "INCREMENT" })}
-        className="btn btn-primary mx-1"
-      >
+      Counter ({counter})
+      <button onClick={() => increment()} className="btn btn-primary mx-1">
         Increment
       </button>
-      <button
-        onClick={() => dispatch({ type: "DECREMENT" })}
-        className="btn btn-primary mx-1"
-      >
-        Decrement
-      </button>
-      <button
-        onClick={() => dispatch({ type: "RESET" })}
-        className="btn btn-primary mx-1"
-      >
+      <button onClick={() => reset()} className="btn btn-primary mx-1">
         Reset
       </button>
     </div>
