ae4967d09c63feb263a702f9d024328ca34776c3
sachinjadhav <sachinjdv7@gmail.com>
2024-07-11 12:27:28 +0530

    implement nested route

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import App from "./App.tsx";
-import "./index.css";
 import "bootstrap/dist/css/bootstrap.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import "./index.css";
+import router from "./routing/routes.tsx";
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <RouterProvider router={router}></RouterProvider>
       <ReactQueryDevtools />
     </QueryClientProvider>
   </React.StrictMode>
---
+++ src/routing/routes.tsx
@@ -0,0 +1,19 @@
+import { createBrowserRouter } from "react-router-dom";
+import HomePage from "./HomePage";
+import Layout from "./Layout";
+import UserDetailPage from "./UserDetailPage";
+import UserListPage from "./UserListPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { path: "", element: <HomePage /> },
+      { path: "users", element: <UserListPage /> },
+      { path: "users/:id", element: <UserDetailPage /> },
+    ],
+  },
+]);
+
+export default router;
